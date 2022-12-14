fc2da17deb72a8d370a4e6b2ce83491908f693d2
prabal255 <2017pcecsprabal115@poornima.org>
2022-12-14 17:01:40 +0530

    Added the homerouting and updated the app component .html

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TobBarComponent } from './tob-bar/tob-bar.component';
-import { TopbarComponent } from './topbar/topbar.component';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LeftbarComponent } from './leftbar/leftbar.component';
@@ -11,8 +10,6 @@ import { RightbarComponent } from './rightbar/rightbar.component';
 
 @NgModule({
   declarations: [
-    TobBarComponent,
-    TopbarComponent,
     HeaderComponent,
     FooterComponent,
     LeftbarComponent,
